a0dce1fb560a2583f21854aea1b9fa3f0a2fc658
steamsy <54457902+steamsyyy@users.noreply.github.com>
2021-01-15 01:02:51 -0600

    Update amd64_nvidia.ps1

--- amd64_nvidia.ps1
+++ amd64_nvidia.ps1
@@ -1,5 +1,8 @@
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-Location "C:\Windows\System32"
+# Install Chocolatey package managewr
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
+# Install applications through Chocolatey
 choco install adobereader firefox adoptopenjdk8 7zip.install `
   notepadplusplus.install vcredist140 vlc git.install `
   nodejs-lts visualstudio2017buildtools filezilla `
@@ -12,4 +15,20 @@ choco install adobereader firefox adoptopenjdk8 7zip.install `
   uplay geforce-experience telnet epicgameslauncher shutup10 `
   sudo zerotier-one msiafterburner rclone
 
+# Latest version working with many python libs that don't support 3.9
 choco install python3 --version=3.8.5.20200721
+
+<#
+  Install BetterDiscordApp (automated version of https://github.com/rauenzi/BetterDiscordApp#manual-installation)
+  Not working as of 1/15/2021 due to dependency issue, https://github.com/rauenzi/BetterDiscordApp/issues/538
+  
+Set-Location "C:\Windows\Temp"
+Invoke-WebRequest -Uri https://github.com/rauenzi/BetterDiscordApp/archive/injector.zip -OutFile "injector.zip"
+Expand-Archive -LiteralPath "C:\Windows\Temp\injector.zip" -DestinationPath "C:\Windows\Temp"
+Rename-Item "C:\Windows\Temp\BetterDiscordApp-injector" "C:\Windows\Temp\app"
+Set-Location $env:LOCALAPPDATA; Set-Location ".\Discord\app*\resources"
+Remove-Item "./app*" -Recurse -ErrorAction Ignore
+Move-Item "C:\Windows\Temp\app" .
+Stop-Process -Name "Discord" -Force
+Start-Process "../discord.exe"
+#>
